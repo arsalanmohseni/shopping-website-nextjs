ebcf676192a318c8d8d9661eb60d2206a82b693d
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-08-10 15:27:59 +0330

    Added use server to apis to just can fetch it

--- app/api/items/route.ts
+++ app/api/items/route.ts
@@ -1,5 +1,7 @@
+'use server';
+
 import { NextResponse } from 'next/server';
 
 export const POST = async () => {
-    return NextResponse.json({msg: "hi, this test succesfully passed"})
+	return NextResponse.json({ msg: 'hi, this test succesfully passed' });
 };
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,5 +1,7 @@
-import { NextResponse } from "next/server";
+'use server';
+
+import { NextResponse } from 'next/server';
 
 export async function POST() {
-    return NextResponse.json({msg: "Hi"})
+	return NextResponse.json({ msg: 'Hi' });
 }
