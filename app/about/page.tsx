24c5737f7cf0e9c73088eb454dda5ce60ff42c93
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-08-13 11:33:52 +0330

    Created about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+const About = () => {
+    return (
+        <div>
+            <h1 className='text-center text-6xl font-bold'>About</h1>
+            <div className="divider"></div>
+            <p className='text-center text-xl'><strong>Do you want to know about this page? No problem! This is a github repo that learn you how to create a real project with Mongodb, Prisma ORM, Next js, React, Redux and Tailwind.</strong></p>
+        </div>
+    );
+};
 
-export default page
+export default About;
