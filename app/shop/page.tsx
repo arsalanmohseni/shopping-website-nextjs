00b58d9168c2c68cb677c420c5b1d09995a28196
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-08-13 12:02:31 +0330

    Completed shop page

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,34 +1,66 @@
-import React from 'react';
-import { useEffect } from 'react';
+'use client';
 
-const page = () => {
-	const requestOptions = {
+import React, { ReactNode } from 'react';
+
+/**
+ * Renders the Shop page with a list of products retrieved from the API.
+ * @returns {ReactNode} The rendered Shop page.
+ */
+const ShopPage = (): React.ReactNode => {
+	interface Product {
+		id: number;
+		name: string;
+		description: string;
+		price: number;
+		image: string;
+		category: string;
+		isAvailable: boolean;
+	}
+
+	const requestOptions: RequestInit = {
 		method: 'GET',
-		redirect: 'follow' as RequestRedirect,
+		redirect: 'follow',
 	};
 
-	useEffect(() => {
-		async function fetchData() {
+	const [products, setProducts] = React.useState<Product[]>([]);
+	const [isLoading, setIsLoading] = React.useState(true);
+
+	React.useEffect(() => {
+		const fetchProducts = async (): Promise<void> => {
 			try {
 				const response = await fetch(
-					'localhost:3000/api/items/',
+					'http://localhost:3000/api/items/',
 					requestOptions,
 				);
-				const products = await response.json();
-				console.log(products);
+				const data = await response.json();
+				setProducts(data);
 			} catch (error) {
-				console.log('error', error);
+				console.error(error);
+			} finally {
+				setIsLoading(false);
 			}
-		}
-		fetchData();
-	});
+		};
+		fetchProducts();
+	}, []);
+
+	if (isLoading) {
+		return <div>Loading...</div>;
+	}
 
 	return (
 		<div>
 			<h1 className="text-center text-6xl font-bold">Shop</h1>
-			<div className="divider"></div>
+			<div className="divider">
+				{products.map((product) => (
+					<div key={product.id}>
+						<h2>{product.name}</h2>
+						<p>{product.description}</p>
+						<p>${product.price}</p>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
 
-export default page;
+export default ShopPage;
