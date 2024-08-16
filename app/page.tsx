1c11f2d579b6481229830871c0da48d756f475e5
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-08-16 13:44:12 +0330

    Added new styles

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ const page = () => {
 			<h1 className="mt-10 text-6xl font-black">
 				A simple e-commerce for practicing React
 			</h1>
-			<h2>Created with ❤️ for beginner, middle and senior developers</h2>
-			<h3>Source code on github link is on the navbar</h3>
+			<h2 className='text-4xl mt-10 font-bold'>Created with ❤️ for beginner, middle and senior developers</h2>
+			<h3 className='text-3xl font-semibold'>Source code on github link is on the navbar</h3>
 		</div>
 	);
 };
