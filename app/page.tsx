ccca1876e134a4c1bbd03b369b4d5c5c61ed9968
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-08-16 13:40:01 +0330

    Created main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const page = () => {
-	return <div>page</div>;
+	return (
+		<div>
+			<h1 className="mt-10 text-6xl font-black">
+				A simple e-commerce for practicing React
+			</h1>
+			<h2>Created with ❤️ for beginner, middle and senior developers</h2>
+			<h3>Source code on github link is on the navbar</h3>
+		</div>
+	);
 };
 
 export default page;
