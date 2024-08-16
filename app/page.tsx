db1e34bf2f5697f1820d4c3db07b5c9df248e044
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-08-16 22:38:20 +0330

    Formated codes

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 const page = () => {
 	return (
-		<div className='*:text-center'>
+		<div className="*:text-center">
 			<h1 className="mt-10 text-6xl font-black">
 				A simple e-commerce for practicing React
 			</h1>
-			<h2 className='text-4xl mt-10 font-bold'>Created with ❤️ for beginner, middle and senior developers</h2>
-			<h3 className='text-3xl font-semibold'>Source code on github link is on the navbar</h3>
+			<h2 className="mt-10 text-4xl font-bold">
+				Created with ❤️ for beginner, middle and senior developers
+			</h2>
+			<h3 className="text-3xl font-semibold">
+				Source code on github link is on the navbar
+			</h3>
 		</div>
 	);
 };
